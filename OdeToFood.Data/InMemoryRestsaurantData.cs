8f47fe1bfbfb756e529c4aa80fda79e15debb967
cransom <cransom@babelstreet.com>
2020-01-29 21:17:50 -0500

    Add project files.

---
+++ OdeToFood.Data/InMemoryRestsaurantData.cs
@@ -0,0 +1,72 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestsaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestsaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id=1, Name="Scott's Pizza", Locaiton="Maryland", Cuisine=CuisineType.Italian},
+                new Restaurant {Id=2, Name="Cinnamon Club", Locaiton="London England", Cuisine=CuisineType.Italian},
+                new Restaurant {Id=3, Name="La Costa", Locaiton="California", Cuisine=CuisineType.Mexican}
+            };
+        }
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            restaurants.Add(newRestaurant);
+            
+            return newRestaurant;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+
+            if(restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Locaiton = updatedRestaurant.Locaiton;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+
+            if(restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+
+            return restaurant;
+        }
+    }
+}
---
+++ OdeToFood.Data/SqlRestaurantData.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using OdeToFood.Core;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace OdeToFood.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            db.Restauruants.Add(newRestaurant);
+            return newRestaurant;
+        }
+
+        public int Commit()
+        {
+            return db.SaveChanges();
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = GetById(id);
+            if(restaurant != null)
+            {
+                db.Restauruants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return db.Restauruants.Find(id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            var query = from r in db.Restauruants
+                        where r.Name.StartsWith(name) || string.IsNullOrEmpty(name)
+                        orderby r.Name
+                        select r;
+
+            return query;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var entity = db.Restauruants.Attach(updatedRestaurant);
+            entity.State = EntityState.Modified;
+            return updatedRestaurant;
+        }
+    }
+}
